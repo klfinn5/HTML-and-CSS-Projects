7ded5d31d1830cbdaf1b4272039d369fd4156a10
Kelsey Finn <klfinn5@gmail.com>
2021-09-03 17:25:18 -0400

    g;itfch

--- portfolio/JS/main.js
+++ portfolio/JS/main.js
@@ -1,6 +1,4 @@
-
-
-  // functions open and contact form
+// functions open and contact form
   function openForm() {
       document.getElementById("myForm").style.display = "block";
   }
@@ -27,9 +25,12 @@
       var slides = document.getElementsByClassName("mySlides");
       var dots = document.getElementById("dot");
       if (n > slides.length) {slideIndex = 1}; // if n (# passed into func) is greater than length of array then index set to 1
-      if (n < 1) {slideIndex = slides.length}; if n less than length slideIndex set to length of slidesz 
+      if (n < 1) {slideIndex = slides.length}; //if n less than length slideIndex set to length of slides 
       for (i = 0; i < slides.length; i++) {
           slides[i].style.display = "none"; //loop each item item in array slides and sets display to none
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "")
       }
       slides[slideIndex - 1].style.display = "block"; //displays img in slideshow
       dots[slideIndex - 1].className += " active"; //adds active styling to dots associated with img
